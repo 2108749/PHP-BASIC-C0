bcf204b78f74f500688089860691c6583f85ed49
Diego <diegoblom01@gmail.com>
2021-01-05 13:55:39 +0100

    opdracht

---
+++ 2-Variabelen-en-datatype/taak08 - data en html combineren/index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+    <body>
+
+        <?php
+        $voornaam1 = "pipo";
+        $voornaam2 = "lol";
+        $voornaam3 = "popi";
+        echo "<li>$voornaam1<li>$voornaam2<li>$voornaam3";
