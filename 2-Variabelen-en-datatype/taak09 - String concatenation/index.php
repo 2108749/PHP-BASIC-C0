2e76c19d95aeb29c558498c5139224d0f2d4a102
Diego <diegoblom01@gmail.com>
2021-01-05 14:01:17 +0100

    opdracht

---
+++ 2-Variabelen-en-datatype/taak09 - String concatenation/index.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <?php
+        $voornaam = "diego";
+        $leeftijd = 17;
+        echo "mijn naam is " . $voornaam . " en ik ben " . $leeftijd . " jaar oud.";
