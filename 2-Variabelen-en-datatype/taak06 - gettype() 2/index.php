69d5d5a7cd0740c7040ba2e87e74451f687cdf65
Diego <diegoblom01@gmail.com>
2021-01-05 13:41:37 +0100

    opdracht

---
+++ 2-Variabelen-en-datatype/taak06 - gettype() 2/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+    <?php
+    $voornaam = "diego";
+    echo gettype($voornaam);
+    $leeftijd = 17;
+    echo gettype($leeftijd);
+    $male = true;
+    echo gettype($male);
