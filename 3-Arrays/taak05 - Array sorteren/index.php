8f2cd7b1a85dfefa37ba9af0743a4a5a461c58d9
Diego <diegoblom01@gmail.com>
2021-01-15 12:54:09 +0100

    opdracht

---
+++ 3-Arrays/taak05 - Array sorteren/index.php
@@ -0,0 +1,10 @@
+<?php
+    $getallen = ['676', '5345', '78657', '3', '46', '21',
+    '47'];
+    asort ($getallen);
+    print_r ($getallen);
+echo "<br>";
+    arsort ($getallen);
+    print_r ($getallen);
+    
+    
---
+++ 4-Arithmetics/taak01 - Arithmetic Operators/index.php
@@ -0,0 +1,11 @@
+<?php
+    $getal1 = 78;
+    $getal2 = 62;
+
+    echo $getal1 + $getal2;
+        echo "<br>";
+    echo $getal2 - $getal1;
+        echo "<br>";
+    echo $getal1 * $getal2;
+        echo "<br>";
+    echo $getal2 / $getal1;
