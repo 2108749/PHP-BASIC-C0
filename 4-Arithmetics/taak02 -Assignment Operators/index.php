62b78ee5be91a69657c8542eae567a17f6a13645
Diego <diegoblom01@gmail.com>
2021-01-15 12:59:27 +0100

    opdracht

---
+++ 4-Arithmetics/taak02 -Assignment Operators/index.php
@@ -0,0 +1,12 @@
+<?php
+
+$getal = 21;
+    echo $getal += 10;
+        echo "<br>";
+    echo $getal -= 10; 
+        echo "<br>";
+    echo $getal /= 10;
+        echo "<br>";
+    echo $getal *= 10;
+
+
