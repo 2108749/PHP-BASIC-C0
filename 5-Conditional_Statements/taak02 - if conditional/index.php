4bedaa2ab93261f458377a4f4f89ca3d660925e4
Diego <diegoblom01@gmail.com>
2021-01-20 12:56:25 +0100

    taak

---
+++ 5-Conditional_Statements/taak02 - if conditional/index.php
@@ -0,0 +1,25 @@
+<?php
+    $variable1 = 10;
+    $variable2 = 6;   
+    if($variable1 != $variable) {
+        echo "<br>";
+        echo "Het is ongelijk";
+    }
+    $test1 = 10;
+    $test2 = 10;
+    if($test1 === $test2){
+        echo "<br>";
+        echo "hetzelfde";
+    }
+    $test1 = 13;
+    $test2 = 12;
+    if($test1 > $test2){
+        echo "<br>";
+        echo "1 is groter dan 2";
+    }
+    $test1 = 10;
+    $test2 = 6;
+    if($test1 >= $test2){
+        echo "<br>";
+        echo "1 is groter of gelijk aan";
+    }
