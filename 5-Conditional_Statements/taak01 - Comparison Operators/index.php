a191ef50f8682adadf6a88dee0566279e2c08c6f
Diego <diegoblom01@gmail.com>
2021-01-20 11:54:18 +0100

    bijwerkingen

--- 5-Conditional_Statements/taak01 - Comparison Operators/index.php
+++ 5-Conditional_Statements/taak01 - Comparison Operators/index.php
@@ -2,3 +2,11 @@
     $variabele1 = 12;
     $variabele2 = 'gewoon tekst';
     var_dump ($variabele1 == $variabele2);
+    var_dump ($variabele1 === $variabele2);
+    var_dump ($variabele1 != $variabele2);
+    var_dump ($variabele1 > $variabele2);
+    var_dump ($variabele1 < $variabele2);
+    var_dump ($variabele1 <> $variabele2);
+    var_dump ($variabele1 => $variabele2);
+    var_dump ($variabele1 <= $variabele2);
+
