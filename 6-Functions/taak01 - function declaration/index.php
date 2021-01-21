bb907b0566a3983ae12883d027fdc8a500ed24e9
Diego <diegoblom01@gmail.com>
2021-01-21 14:51:47 +0100

    opdracht

---
+++ 6-Functions/taak01 - function declaration/index.php
@@ -0,0 +1,8 @@
+<?php
+    function zetTweeWoordenAchterElkaar(){
+        $woordje = 'hallo';
+        $zinnetje = 'mooie blauwe planeet';
+        $helezin = 'Hallo, mooie blauwe planeet';
+        echo $helezin;
+    }
+    zetTweeWoordenAchterElkaar();
