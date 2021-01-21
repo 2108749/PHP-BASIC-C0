847f95d13ee1483d2ce4ecf743ba017f4ca7c422
Diego <diegoblom01@gmail.com>
2021-01-21 14:43:08 +0100

    opdracht

---
+++ 5-Conditional_Statements/taak06 - Logical Operators/index.php
@@ -0,0 +1,15 @@
+<?php
+    $getal = 22;
+    $cijfer = 11;
+
+    if($getal < 22 && $getal > 22) {
+        echo "Het getal is kleiner of groter dan 22";
+    } else {
+        echo "het getal is hetzelfde";
+    }
+    echo "<br>";
+    if($cijfer < 11 && $cijfer >20) {
+        echo "het is een of klein getal of groot getal";
+    } else {
+        echo "het zit tussen 11 en 20";
+    } 
