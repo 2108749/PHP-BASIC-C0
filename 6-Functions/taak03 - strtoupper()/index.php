1b9dfb114eeadebbc1e43e9024cdfaaafccdfee2
Diego <diegoblom01@gmail.com>
2021-01-21 15:11:55 +0100

    opdracht

---
+++ 6-Functions/taak03 - strtoupper()/index.php
@@ -0,0 +1,8 @@
+<?php
+    function telAlleWoorden(){
+        $mijn = "Mijn blauwe auto is een mooie snelle wagen";
+        strlen($mijn);
+        $lengtezin = 42;
+        echo $lengtezin;
+    }
+    telAlleWoorden();
