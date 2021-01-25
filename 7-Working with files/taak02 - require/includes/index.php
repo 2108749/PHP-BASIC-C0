8e1ba12e7108637b9c3b404ca96a23651436335c
Diego <diegoblom01@gmail.com>
2021-01-25 14:53:08 +0100

    opdracht

--- 7-Working with files/taak02 - require/includes/index.php
+++ 7-Working with files/taak02 - require/includes/index.php
@@ -4,9 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <?php require 'C:\Users\diego\OneDrive - ROCvA, ROCvF en VOvA\jaar1\php-basic-c0\7-Working with files\taak02 - require\includes\database.php';
+    
+    ?>
 </head>
 <body>
-    <?php require 'C:\Users\diego\OneDrive - ROCvA, ROCvF en VOvA\jaar1\php-basic-c0\7-Working with files\taak02 - require\includes\database.php';
-    ?>
+    <?php include 'C:\Users\diego\OneDrive - ROCvA, ROCvF en VOvA\jaar1\php-basic-c0\7-Working with files\taak02 - require\includes\menu.php';
+?>
 </body>
 </html>
